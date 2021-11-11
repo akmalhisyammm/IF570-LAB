84a7c5434e7226283489a9de3403cec857a386a0
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-11 11:53:20 +0700

    add: week 10 submission

---
+++ Week10_MuhammadAkmalHisyam_40027/Week10C_40027/app/src/main/java/id/ac/umn/week10c_40027/CustomService.java
@@ -0,0 +1,40 @@
+package id.ac.umn.week10c_40027;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CustomService extends Service {
+    public CustomService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        Log.i("CUSTOMSERVICE", "onBind: Service Bind");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("CUSTOMSERVICE", "onCreate: CustomService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("CUSTOMSERVICE", "onStartCommand: " + startId);
+
+        AsyncTask customServiceTask = new CustomServiceTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, startId);
+
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("CUSTOMSERVICE", "onDestroy: Service Destroyed");
+    }
+}
---
+++ Week10_MuhammadAkmalHisyam_40027/Week10C_40027/app/src/main/java/id/ac/umn/week10c_40027/CustomServiceTask.java
@@ -0,0 +1,33 @@
+package id.ac.umn.week10c_40027;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class CustomServiceTask extends AsyncTask<Integer, Integer, Integer> {
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        Log.i("CUSTOMSERVICE", "onStartCommand: " + values[0] + " berjalan " + values[1] + " persen");
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        Log.i("CUSTOMSERVICE", "onStartCommand: " + integer + " Selesai");
+    }
+
+    @Override
+    protected Integer doInBackground(Integer... integers) {
+        int startId = integers[0];
+        int n = (int)(Math.random() * 50) + 10;
+
+        try {
+            for (int i = 0; i < n; i++) {
+                Thread.sleep(200);
+                publishProgress(startId, ((int)((100 * i) / (float)n)));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return startId;
+    }
+}
